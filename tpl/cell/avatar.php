b89e841848ef4a4db5b9748341c0caec48d55aee
gizmore <gizmore@wechall.net>
2017-10-15 22:46:43 +0200

    6.06_layout

--- tpl/cell/avatar.php
+++ tpl/cell/avatar.php
@@ -2,5 +2,4 @@
 <div class="gdo-avatar <?=$field->user->getGender()?>">
   <img alt="<?= t('avatar_of', [$field->user->displayNameLabel()]); ?>"
    src="<?= href('Avatar', 'Image', '&ajax=1&file=' . $field->gdo->getVar('avatar_file_id')); ?>" />
-   yyy
 </div>
