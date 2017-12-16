af62ad3a0b7e32f6656b73eb1cb5db1c9a5c382e
gizmore <gizmore@wechall.net>
2017-12-16 15:31:31 +0100

    6.07_sync

--- GDT_Avatar.php
+++ GDT_Avatar.php
@@ -9,7 +9,7 @@ final class GDT_Avatar extends GDT_ObjectSelect
 	public function __construct()
 	{
 		$this->table(GDO_Avatar::table());
-		$this->currentUser();
+// 		$this->currentUser();
 	}
 	
 	/**
