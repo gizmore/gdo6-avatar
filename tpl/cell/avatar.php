d278d318b5ee0cbbb89a97a77b7661f6d5340236
gizmore <gizmore@wechall.net>
2018-04-02 22:10:53 +0200

    6.07

--- tpl/cell/avatar.php
+++ tpl/cell/avatar.php
@@ -1,5 +1,7 @@
-<?php /** @var $field \GDO\Avatar\GDT_Avatar **/ ?>
+<?php /** @var $field \GDO\Avatar\GDT_Avatar **/
+use GDO\Avatar\GDO_Avatar;
+?>
 <div class="gdo-avatar <?=$field->user->getGender()?>">
   <img alt="<?= t('avatar_of', [$field->user->displayNameLabel()]); ?>"
-   src="<?= href('Avatar', 'Image', '&ajax=1&file=' . $field->gdo->getVar('avatar_file_id')); ?>" />
+   src="<?= href('Avatar', 'Image', '&ajax=1&file=' . GDO_Avatar::forUser($field->user)->getFileID()); ?>" />
 </div>
