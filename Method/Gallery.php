b6a3daf503106d149bfd2051281fafb3ac82a780
gizmore <gizmore@wechall.net>
2020-11-22 21:44:23 +0100

    6.10-r9381_CKEditor_and_Prism

--- Method/Gallery.php
+++ Method/Gallery.php
@@ -2,11 +2,13 @@
 namespace GDO\Avatar\Method;
 
 use GDO\Table\MethodList;
+use GDO\Avatar\GDO_Avatar;
 
 final class Gallery extends MethodList
 {
-	public function execute()
-	{
-		
-	}
+    public function gdoTable()
+    {
+        return GDO_Avatar::table();
+    }
+
 }
